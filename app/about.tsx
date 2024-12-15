e48d7d0c93896293e4ad0600c3b6a8563ae54e2a
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2024-12-15 17:04:55 +0530

    added assets and splash screen

--- app/about.tsx
+++ app/about.tsx
@@ -1,23 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 const About = () => {
   return (
     <>
-      <View style={styles.container}>
-        <Text>About</Text>
+      <View  className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl">About</Text>
       </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-    container: {
-      display: 'flex',
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
-    }
-  })
+
 
 export default About;
