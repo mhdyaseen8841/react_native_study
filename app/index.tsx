992afcab2d98cdc1bf6761a5cf79a3c6665eb9d5
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2024-12-15 15:48:54 +0530

    native wind added

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import {Link} from 'expo-router'
+import "../global.css"
 const Index = () => {
   return (
     <>
-      <View style={styles.container}>
-        <Text>Hi</Text>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl">Hi</Text>
         <Link href="/about" style={{color: 'blue'}}>Go to About</Link>
       </View>
     </>
